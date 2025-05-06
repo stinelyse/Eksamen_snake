47f449c0a2274dbb14f89a8315c5ed23f8ea6314
stine Lyse <stinely@uia.no>
2025-05-06 15:07:40 +0200

    all menyen fungerer

---
+++ Hovedoppgave_2025_O/module_6/snake/game.mjs
@@ -0,0 +1,181 @@
+"use strict";
+
+//-----------------------------------------------------------------------------------------
+//----------- Import modules, mjs files  ---------------------------------------------------
+//-----------------------------------------------------------------------------------------
+import libSprite from "../../common/libs/libSprite_v2.mjs";
+import { TGameBoard, GameBoardSize, TBoardCell } from "./gameBoard.mjs";
+import { TSnake, EDirection} from "./snake.mjs";
+import { TBait } from "./bait.mjs";
+import { TMenu } from "./menu.mjs";
+
+//-----------------------------------------------------------------------------------------
+//----------- variables and object --------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+const cvs = document.getElementById("cvs");
+const spcvs = new libSprite.TSpriteCanvas(cvs);
+let gameSpeed = 4; // Game speed multiplier;
+let hndUpdateGame = null;
+export const EGameStatus = { Idle: 0, Playing: 1, Pause: 2, GameOver: 3 };
+
+// prettier-ignore
+export const SheetData = {
+  Head:     { x:   0, y:   0, width:  38, height:  38, count:  4 },
+  Body:     { x:   0, y:  38, width:  38, height:  38, count:  6 },
+  Tail:     { x:   0, y:  76, width:  38, height:  38, count:  4 },
+  Bait:     { x:   0, y: 114, width:  38, height:  38, count:  1 },
+  Play:     { x:   0, y: 155, width: 202, height: 202, count: 10 },
+  GameOver: { x:   0, y: 647, width: 856, height: 580, count:  1 },
+  Home:     { x:  65, y: 995, width: 169, height: 167, count:  1 },
+  Retry:    { x: 614, y: 995, width: 169, height: 167, count:  1 },
+  Resume:   { x:   0, y: 357, width: 202, height: 202, count: 10 },
+  Number:   { x:   0, y: 560, width:  81, height:  86, count: 10 },
+};
+
+export const GameProps = {
+  gameBoard: null,
+  gameStatus: EGameStatus.Idle,
+  snake: null,
+  bait: null, 
+  menu: null,
+  score: 0,
+};
+
+//------------------------------------------------------------------------------------------
+//----------- Exported functions -----------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+export function newGame() {
+  GameProps.gameBoard = new TGameBoard();
+  GameProps.snake = new TSnake(spcvs, new TBoardCell(5, 5)); // Initialize snake with a starting position
+  GameProps.bait = new TBait(spcvs); // Initialize bait with a starting position
+  gameSpeed = 4; // Reset game speed
+  GameProps.score = 0; //resetter når nytt game
+  
+}
+
+export function baitIsEaten() {
+
+  GameProps.score +=10;
+  
+  console.log("Bait eaten!");
+  GameProps.bait.update(); //spawner ny bait
+  
+  // score++
+
+  increaseGameSpeed(); // Increase game speed
+}
+
+
+//------------------------------------------------------------------------------------------
+//----------- functions -------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+function loadGame() {
+  cvs.width = GameBoardSize.Cols * SheetData.Head.width;
+  cvs.height = GameBoardSize.Rows * SheetData.Head.height;
+
+  GameProps.gameStatus = EGameStatus.Playing; // change game status to Idle
+
+  /* Create the game menu here */ 
+  GameProps.menu = new TMenu(spcvs);
+  newGame();
+
+  requestAnimationFrame(drawGame);
+  console.log("Game canvas is rendering!");
+  hndUpdateGame = setInterval(updateGame, 1000 / gameSpeed); // Update game every 1000ms / gameSpeed
+  console.log("Game canvas is updating!");
+}
+
+function drawGame() {
+  // Clear the canvas
+  spcvs.clearCanvas();
+
+  switch (GameProps.gameStatus) {
+    case EGameStatus.Playing:
+      GameProps.menu.draw();
+      GameProps.snake.draw();
+      GameProps.bait.draw();
+      break;
+    case EGameStatus.Pause:
+      GameProps.bait.draw();
+      GameProps.snake.draw();
+      GameProps.menu.draw();
+      break;
+    case EGameStatus.Idle:
+      GameProps.menu.draw();
+      break;
+    case EGameStatus.GameOver:
+      GameProps.menu.draw();
+      break;
+  }
+  // Request the next frame
+  requestAnimationFrame(drawGame);
+}
+
+function updateGame() {
+  // Update game logic here
+  switch (GameProps.gameStatus) {
+    case EGameStatus.Playing:
+      if (!GameProps.snake.update()) {
+        GameProps.gameStatus = EGameStatus.GameOver;
+        console.log("Game over!");
+      }
+      break;
+  }
+  
+}
+
+
+function increaseGameSpeed() {
+  /* Increase game speed logic here */
+  console.log("Increase game speed!");
+}
+
+export function startGame(){
+  newGame();
+  GameProps.gameStatus = EGameStatus.Playing;
+}
+
+export function homeScreen(){
+  GameProps.gameStatus = EGameStatus.Idle;
+}
+
+export function resumeGame(){
+  GameProps.gameStatus = EGameStatus.Playing;
+}
+//-----------------------------------------------------------------------------------------
+//----------- Event handlers --------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+
+function onKeyDown(event) {
+  switch (event.key) {
+    case "ArrowUp":
+      GameProps.snake.setDirection(EDirection.Up);
+      break;
+    case "ArrowDown":
+      GameProps.snake.setDirection(EDirection.Down);
+      break;
+    case "ArrowLeft":
+      GameProps.snake.setDirection(EDirection.Left);
+      break;
+    case "ArrowRight":
+      GameProps.snake.setDirection(EDirection.Right);
+      break;
+    case " ":
+      if (GameProps.gameStatus === EGameStatus.Playing) {
+        GameProps.gameStatus = EGameStatus.Pause;
+      }
+      /* Pause the game logic here */
+      
+      break;
+    default:
+      console.log(`Key pressed: "${event.key}"`);
+  }
+}
+//-----------------------------------------------------------------------------------------
+//----------- main -----------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+
+spcvs.loadSpriteSheet("./Media/spriteSheet.png", loadGame);
+document.addEventListener("keydown", onKeyDown);
---
+++ Hovedoppgave_2025_O/module_6/snake/menu.mjs
@@ -0,0 +1,106 @@
+"use strict";
+import libSprite_v2 from "../../common/libs/libSprite_v2.mjs";
+import lib2d_v2 from "../../common/libs/lib2d_v2.mjs";
+import { SheetData, GameProps, EGameStatus} from "./game.mjs";
+import { startGame, homeScreen, resumeGame } from "./game.mjs";
+/* Use this file to create the menu for the snake game. */
+
+export class TMenu { //når elementene i klassen er private #, så må draw ligge inne i klassen.
+    #spButtonPlay;
+    #spGameOver;
+    #spResume;
+    #spRestart;
+    #spGameOverScore;
+    #spPlayScore;
+    #spHome;
+    #spcvs;
+    #baitScore;
+    
+    constructor(aSpriteCanvas) {
+        this.#spcvs = aSpriteCanvas;
+        GameProps.gameStatus = EGameStatus.Idle;
+
+        const possp = new lib2d_v2.TPosition(350, 230); //utgangspunkt posisjon for alt
+
+        pos.y = 230;
+        pos.x= 350;
+        this.#spButtonPlay = new libSprite_v2.TSpriteButton(aSpriteCanvas, SheetData.Play, pos);
+        this.#spButtonPlay.onClick = startGame;
+
+        pos.y = 50;
+        pos.x= 30;
+        this.#spGameOver = new libSprite_v2.TSprite(aSpriteCanvas, SheetData.GameOver, pos);
+
+        pos.y = 230;
+        pos.x= 350;
+        this.#spResume = new libSprite_v2.TSpriteButton(aSpriteCanvas, SheetData.Resume, pos);
+        this.#spResume.onClick = resumeGame;
+
+        pos.y = 230;
+        pos.x= 350;
+        this.#baitScore= new libSprite_v2.TSpriteNumber(aSpriteCanvas, SheetData.Number, pos);
+        this.#baitScore.scale = 0.5;
+        this.#baitScore.alpha = 0.5;
+        this.#baitScore.value = 50; //value er score poeng.
+
+
+        pos.y = 398;
+        pos.x= 645;
+        this.#spRestart = new libSprite_v2.TSpriteButton(aSpriteCanvas, SheetData.Retry, pos);
+        this.#spRestart.onClick = startGame;
+
+        pos.y = 398;
+        pos.x= 95;
+        this.#spHome = new libSprite_v2.TSpriteButton(aSpriteCanvas, SheetData.Home, pos);
+        this.#spHome.onClick = homeScreen;
+
+
+    }
+
+draw() {
+    switch (GameProps.gameStatus) {
+      case EGameStatus.Idle:
+        this.#spButtonPlay.draw();
+        break;
+      case EGameStatus.GameOver:
+        this.#spGameOver.draw();
+        this.#spRestart.draw();
+        //this.#spScore.draw();
+        this.#spHome.draw();
+        break;
+      case EGameStatus.Playing:
+        this.#baitScore.draw();
+        break;
+      case EGameStatus.Pause:
+        this.#spResume.draw();
+        break;
+        }   
+    }
+
+    get baitValue(){
+      return this.#baitScore.value;
+      
+    }
+
+    set baitValue(aValue){
+      if(this.#baitScore.value> 1){
+        this.#baitScore.value = aValue;
+      }
+     
+    } // virker for -- 
+
+
+
+   /*  onClick(event){
+      if (GameProps.gameStatus === EGameStatus.Idle){
+        this.#spButtonPlay.checkClick(event);
+      }
+      if (GameProps.gameStatus === EGameStatus.GameOver){
+        this.#spHome.checkClick(event);
+        this.#spRestart.checkClick(event);
+      }
+      if (GameProps.gameStatus === EGameStatus.Pause){
+        this.#spResume.checkClick(event);
+      }
+    } */
+} 
